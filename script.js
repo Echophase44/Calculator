4e115d73b96d3550289fad9b313a70eac4f76411
Nick <Echophase44@github.com>
2022-04-14 06:11:04 -0400

    Added id names to HTML and const document selectors to JS

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+//////////////////// Numbers ///////////////////
+const one = document.querySelector("#one-btn");
+const two = document.querySelector("#two-btn");
+const three = document.querySelector("#three-btn");
+const four = document.querySelector("#four-btn");
+const five = document.querySelector("#five-btn");
+const six = document.querySelector("#six-btn");
+const seven = document.querySelector("#seven-btn");
+const eight = document.querySelector("#eight-btn");
+const nine = document.querySelector("#nine-btn");
+const zero = document.querySelector("#zero-btn");
+const dot = document.querySelector("#dot-btn");
+
+////////////////////// Operators ////////////////////
+const equalBtn = document.querySelector("#equal-btn");
+const clearBtn = document.querySelector("#clear-btn");
+const divideBtn = document.querySelector("#divide-btn");
+const multiplyBtn = document.querySelector("#multiply-btn");
+const subtractBtn = document.querySelector("#subtract-btn");
+const addBtn = document.querySelector("#add-btn");
+
 function add(a, b) {
   return a + b;
 }
@@ -15,3 +36,9 @@ function divide(a, b) {
 }
 
 console.log(add(4, 6));
+
+function sendtoConsole(input) {
+  console.log(7);
+}
+
+seven.addEventListener("click", sendtoConsole);
