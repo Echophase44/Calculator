ac74e1f31a32a0a57d635347110ad02568af913f
Nick <Echophase44@github.com>
2022-04-18 22:22:43 -0400

    Updates to HTML and JS - applied event listeners for number buttons and have them update the display

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
 //////////////////// Numbers ///////////////////
-const one = document.querySelector("#one-btn");
-const two = document.querySelector("#two-btn");
-const three = document.querySelector("#three-btn");
-const four = document.querySelector("#four-btn");
-const five = document.querySelector("#five-btn");
-const six = document.querySelector("#six-btn");
-const seven = document.querySelector("#seven-btn");
-const eight = document.querySelector("#eight-btn");
-const nine = document.querySelector("#nine-btn");
-const zero = document.querySelector("#zero-btn");
-const dot = document.querySelector("#dot-btn");
-
+const numberBtns = document.querySelectorAll(".number");
+const displayOutput = document.querySelector(".displayOutput");
+const displayHistory = document.querySelector(".displayHistory");
 ////////////////////// Operators ////////////////////
-const equalBtn = document.querySelector("#equal-btn");
-const clearBtn = document.querySelector("#clear-btn");
-const divideBtn = document.querySelector("#divide-btn");
-const multiplyBtn = document.querySelector("#multiply-btn");
-const subtractBtn = document.querySelector("#subtract-btn");
-const addBtn = document.querySelector("#add-btn");
 
 function add(a, b) {
   return a + b;
@@ -35,10 +20,13 @@ function divide(a, b) {
   return a / b;
 }
 
-console.log(add(4, 6));
-
-function sendtoConsole(input) {
-  console.log(7);
+for (let i = 0; i < numberBtns.length; i++) {
+  numberBtns[i].addEventListener("click", () => {
+    console.log(numberBtns[i].innerText);
+    updateDisplay(numberBtns[i].innerText);
+  });
 }
 
-seven.addEventListener("click", sendtoConsole);
+function updateDisplay(input) {
+  displayOutput.innerText += input;
+}
